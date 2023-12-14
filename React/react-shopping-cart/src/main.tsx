34753b29489c3ba5773313c8c355654bb8e08aca
Yash <yash.mehta@jklgroup.in>
2023-12-14 10:51:48 +0530

    14/12/2023

--- React/react-shopping-cart/src/main.tsx
+++ React/react-shopping-cart/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);
