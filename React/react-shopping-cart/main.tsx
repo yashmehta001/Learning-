52946efb1b99bfcf06917d767a5114c73757b878
Yash <yash.mehta@jklgroup.in>
2023-12-14 12:09:02 +0530

    14/12/2023

---
+++ React/react-shopping-cart/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import React from "react";
+
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>
+);
