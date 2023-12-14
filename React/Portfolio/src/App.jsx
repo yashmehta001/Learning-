5e013ed2bf3a1a4e5af5691b36e1cc00d7726eb5
Yash Mehta <yashmehta001@yahoo.in>
2023-12-14 21:44:17 +0530

    14/12/2023

--- React/Portfolio/src/App.jsx
+++ React/Portfolio/src/App.jsx
@@ -1,19 +1,3 @@
-//Styles
-import { Wrapper } from "./App.styles";
-
-export type CartItemType = {
-  id: number;
-  category: string;
-  description: string;
-  image: string;
-  price: number;
-  title: string;
-  amount: number;
-};
-
-const getProducts = async (): Promise<CartItemType> =>
-  await (await fetch("https://fakestoreapi.com/products")).json();
-
 function App() {
   return <>Start</>;
 }
