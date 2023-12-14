a7c426e9722e018c05154528fc329575976ddbed
Yash Mehta <yashmehta001@yahoo.in>
2023-12-14 22:45:16 +0530

    14/12/2023

--- React/Portfolio/src/App.jsx
+++ React/Portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
-  return (<h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>);
+  return (
+    <h1 className="text-xl text-yellow-500 font-semibold">Hello world!</h1>
+  );
 }
 
 export default App;
