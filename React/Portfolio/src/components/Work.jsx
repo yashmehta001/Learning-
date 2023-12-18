8d4b933e81df4c1b6a4cae763974deb646f1a16d
Yash <yash.mehta@jklgroup.in>
2023-12-18 18:37:41 +0530

    work

---
+++ React/Portfolio/src/components/Work.jsx
@@ -0,0 +1,50 @@
+import AWS from "../assets/aws.png";
+function Work() {
+  return (
+    <div className="w-full md:h-screen bg-[#0a192f] text-gray-300" name="work">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">Check out some of my recent work</p>
+        </div>
+        {/* container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${AWS})` }}
+            className="shadow-lg shadow-[#040C16] group container rounded-md text-center flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
