f5dffdf1d9d37685dea0f81c248b6b16b3c01e4e
Yash <yash.mehta@jklgroup.in>
2023-12-27 09:48:25 +0530

    added google oauth

--- React/oauth/src/App.jsx
+++ React/oauth/src/App.jsx
@@ -1,5 +1,6 @@
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
@@ -9,8 +10,13 @@ function App() {
         <div>
           Hello
           <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
+            onSuccess={async (credentialResponse) => {
+              console.log(credentialResponse)
+              const response = await axios.post("http://localhost:3000/auth/google/login", {
+                token: credentialResponse.credential,
+              });
+              const data = response.data;
+              console.log(data);
             }}
             onError={() => {
               console.log("Login Failed");
