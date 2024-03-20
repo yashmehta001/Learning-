4a6d3d24054a88a34f4f458e5534927cd8faa2d6
yash <yashmehta766@gmail.com>
2024-03-20 19:49:40 +0530

    Navbar completed

--- React/gym-typescript/src/App.tsx
+++ React/gym-typescript/src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NavBar from './scenes/navbar'
+import { SelectedPage } from './shared/types';
+
+
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home')
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  )
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(()=>{
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const handleScroll =()=>{
+      if(window.scrollY === 0){
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home);
+      } 
+      if(window.scrollY !== 0 ) setIsTopOfPage(false);
+
+      window.addEventListener('scroll', handleScroll);
+      return ()=> window.removeEventListener("scroll", handleScroll)
+    }
+  },[]);
   return (
   <>
-    <NavBar 
+    <NavBar
+      isTopOfPage ={isTopOfPage}
       selectedPage={selectedPage} 
       setSelectedPage={setSelectedPage}
     />
