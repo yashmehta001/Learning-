b01559d6fc75e31e161133078b3598bdd9116222
yash <yashmehta766@gmail.com>
2024-03-25 21:55:17 +0530

    removed app to move to seperate repository

--- React/Portfolio/src/services/supabase.js
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://kzztysktrqtmworrvfbz.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPERBASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
